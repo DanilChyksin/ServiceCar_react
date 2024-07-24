a2dcb1c14576c92980bcbf1e7162c34360d1061f
Danil <chyksin@bk.ru>
2024-07-24 14:06:25 +0700

    comit

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,20 @@ import { getFAQService } from "./assets/salonSlice";
 import { useEffect } from "react";
 
 function App() {
-  // const dispatch = useDispatch();
-  // const data = async () => {
-  //   try {
-  //     const response = await fetch(`https://testingnil1.ru/assets/output.json`);
-  //     const data = await response.json();
-  //     return data;
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  // console.log(data());
-  // useEffect(() => {
-  //   dispatch(getFAQService());
-  // }, [dispatch]);
+  const dispatch = useDispatch();
+  const data = async () => {
+    try {
+      const response = await fetch(`https://testingnil1.ru/assets/output.json`);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(data());
+  useEffect(() => {
+    dispatch(getFAQService());
+  }, [dispatch]);
   return <AppRoutes />;
 }
 
--- src/assets/salonSlice.js
+++ src/assets/salonSlice.js
@@ -1,19 +1,19 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-// const BASE_URL = "https://testingnil1.ru/assets/output.json";
+const BASE_URL = "https://testingnil1.ru/assets/output.json";
 
-// export const getFAQService = createAsyncThunk(
-//   "service/getService",
-//   async (_, thunkAPI) => {
-//     try {
-//       const response = await fetch(`${BASE_URL}`);
-//       const data = await response.json();
-//       return data;
-//     } catch (err) {
-//       console.log(err);
-//       return thunkAPI.rejectWithValue(err);
-//     }
-//   }
-// );
+export const getFAQService = createAsyncThunk(
+  "service/getService",
+  async (_, thunkAPI) => {
+    try {
+      const response = await fetch(`${BASE_URL}`);
+      const data = await response.json();
+      return data;
+    } catch (err) {
+      console.log(err);
+      return thunkAPI.rejectWithValue(err);
+    }
+  }
+);
 const salonSlice = createSlice({
   name: "salon",
   initialState: {
@@ -30,18 +30,18 @@ const salonSlice = createSlice({
       state.salon = payload;
     },
   },
-  // extraReducers: (builder) => {
-  //   builder.addCase(getFAQService.pending, (state) => {
-  //     state.isLoading = true;
-  //   });
-  //   builder.addCase(getFAQService.fulfilled, (state, action) => {
-  //     state.FAQservice = action.payload;
-  //     state.isLoading = false;
-  //   });
-  //   builder.addCase(getFAQService.rejected, (state) => {
-  //     state.isLoading = false;
-  //   });
-  // },
+  extraReducers: (builder) => {
+    builder.addCase(getFAQService.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getFAQService.fulfilled, (state, action) => {
+      state.FAQservice = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(getFAQService.rejected, (state) => {
+      state.isLoading = false;
+    });
+  },
 });
 export default salonSlice.reducer;
 export const { addCityName, addSalonName } = salonSlice.actions;
